1c319c80afe212a311cb7b7dda9c0b6fb994cc8d
Farman hafeez <farmanhafeezj@gmail.com>
2020-06-25 11:54:57 +0530

    employee documentation

--- api/employee/index.php
+++ api/employee/index.php
@@ -1,10 +1,10 @@
 <?php
 
-$data = file_get_contents('user.json');
+$data = file_get_contents('employee.json');
 $array = json_decode($data, true);
 shuffle($array);
 $arraypush = array(
-    "users" => array()
+    "employee" => array()
 );
 
 $result = isset($_GET['result']) ? $_GET['result'] : null;
@@ -17,44 +17,35 @@ if ($result > 2000) {
     echo json_encode(array("error" => "Something went wrong!"));
 } elseif ($result) {
     for ($i = 0; $i < $result; $i++) {
-        array_push($arraypush['users'], $array[$i]);
+        array_push($arraypush['employee'], $array[$i]);
     }
 } elseif ($gender) {
     for ($i = 0; $i < count($array); $i++) {
         if ($gender == $array[$i]['gender']) {
-            array_push($arraypush['users'], $array[$i]);
+            array_push($arraypush['employee'], $array[$i]);
         }
     }
 } elseif ($country) {
     for ($i = 0; $i < count($array); $i++) {
         if ($country == $array[$i]['country']) {
-            array_push($arraypush['users'], $array[$i]);
+            array_push($arraypush['employee'], $array[$i]);
         }
     }
 } elseif ($minage && $maxage) {
     for ($i = 0; $i < count($array); $i++) {
         if ($minage < $array[$i]['age'] && $maxage > $array[$i]['age']) {
-            array_push($arraypush['users'], $array[$i]);
+            array_push($arraypush['employee'], $array[$i]);
         }
     }
 } else {
     for ($i = 0; $i < count($array); $i++) {
-        array_push($arraypush['users'], $array[$i]);
+        array_push($arraypush['employee'], $array[$i]);
     }
 }
 
 // Remove ID from the array
-for ($i = 0; $i < count($arraypush['users']); $i++) {
-    unset($arraypush['users'][$i]['id']);
+for ($i = 0; $i < count($arraypush['employee']); $i++) {
+    unset($arraypush['employee'][$i]['id']);
 }
 
 echo json_encode($arraypush, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-
-// for ($i = 0; $i < count($array); $i++) {
-//     if (in_array($array[$i]['country'], $arraypush['users'])) {
-//         continue;
-//     }
-//     array_push($arraypush['users'], $array[$i]['country']);
-// }
-
-// echo json_encode($arraypush['users']);
