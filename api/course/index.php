2f22ce1fe66097922660aa4d8d788451914105ca
Farman hafeez <farmanhafeezj@gmail.com>
2020-06-25 13:17:42 +0530

    course documentation

--- api/course/index.php
+++ api/course/index.php
@@ -1,60 +1,36 @@
 <?php
 
-$data = file_get_contents('user.json');
+$data = file_get_contents('course.json');
 $array = json_decode($data, true);
 shuffle($array);
 $arraypush = array(
-    "users" => array()
+    "course" => array()
 );
 
 $result = isset($_GET['result']) ? $_GET['result'] : null;
-$gender = isset($_GET['gender']) ? $_GET['gender'] : null;
-$country = isset($_GET['country']) ? $_GET['country'] : null;
-$minage = isset($_GET['minage']) ? $_GET['minage'] : null;
-$maxage = isset($_GET['maxage']) ? $_GET['maxage'] : null;
+$department = isset($_GET['department']) ? $_GET['department'] : null;
 
 if ($result > 2000) {
     echo json_encode(array("error" => "Something went wrong!"));
 } elseif ($result) {
     for ($i = 0; $i < $result; $i++) {
-        array_push($arraypush['users'], $array[$i]);
+        array_push($arraypush['course'], $array[$i]);
     }
-} elseif ($gender) {
+} elseif ($department) {
     for ($i = 0; $i < count($array); $i++) {
-        if ($gender == $array[$i]['gender']) {
-            array_push($arraypush['users'], $array[$i]);
-        }
-    }
-} elseif ($country) {
-    for ($i = 0; $i < count($array); $i++) {
-        if ($country == $array[$i]['country']) {
-            array_push($arraypush['users'], $array[$i]);
-        }
-    }
-} elseif ($minage && $maxage) {
-    for ($i = 0; $i < count($array); $i++) {
-        if ($minage < $array[$i]['age'] && $maxage > $array[$i]['age']) {
-            array_push($arraypush['users'], $array[$i]);
+        if ($department == $array[$i]['department_abr']) {
+            array_push($arraypush['course'], $array[$i]);
         }
     }
 } else {
     for ($i = 0; $i < count($array); $i++) {
-        array_push($arraypush['users'], $array[$i]);
+        array_push($arraypush['course'], $array[$i]);
     }
 }
 
 // Remove ID from the array
-for ($i = 0; $i < count($arraypush['users']); $i++) {
-    unset($arraypush['users'][$i]['id']);
+for ($i = 0; $i < count($arraypush['course']); $i++) {
+    unset($arraypush['course'][$i]['id']);
 }
 
 echo json_encode($arraypush, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-
-// for ($i = 0; $i < count($array); $i++) {
-//     if (in_array($array[$i]['country'], $arraypush['users'])) {
-//         continue;
-//     }
-//     array_push($arraypush['users'], $array[$i]['country']);
-// }
-
-// echo json_encode($arraypush['users']);
